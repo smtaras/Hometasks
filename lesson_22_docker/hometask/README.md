d328169fca8f8c7b9c1c45a8873550da43290ee2
Smakouz Taras <smakouztaras123@gmail.com>
2026-06-14 18:28:41 +0300

    commit

---
+++ lesson_22_docker/hometask/README.md
@@ -0,0 +1,9 @@
+## Example of processing
+
+![exmpl1 ](media/1.png)
+
+![exmpl2 ](media/2.jpg)
+
+![exmpl3 ](media/3.png)
+
+![exmpl4 ](media/4.png)
---
+++ lesson_22_docker/hometask/app.py
@@ -0,0 +1,21 @@
+import io
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from transformers import pipeline
+
+app = FastAPI()
+
+classifier = pipeline("image-classification", model="google/mobilenet_v2_1.0_224")
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
+    
+    results = classifier(image)
+    
+    return {"label": results[0]['label'], "confidence": results[0]['score']}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
